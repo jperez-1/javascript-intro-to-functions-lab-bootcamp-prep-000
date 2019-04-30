815004b7e4faa1bfa3a935ec1b5c266fc57254b9
Justin Perez <jperez.devkaizen@gmail.com>
2019-04-30 15:32:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) return("I can't hear you!");
-  else if (string === string.toUpperCase()) return("YES INDEED!");
-  else if (string === string.mixedCase()) return("I love you, too.");
+  if (string === string.toLowerCase()) { 
+    return("I can't hear you!");
+  } else if (string === string.toUpperCase()) { 
+    return("YES INDEED!");
+  } else (string === string.mixedCase()) { 
+    return("I love you, too.");
 }
